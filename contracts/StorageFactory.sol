072da795075617831539bcdf8e56ab20c5a31fc5
bejzik8 <bejzik8@gmail.com>
2023-10-25 03:28:56 +0200

    Updated StorageFactory contract.

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -15,13 +15,10 @@ contract StorageFactory {
         // Address
         // ABI - Aplication Binary Interface
 
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        simpleStorage.store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-
-        return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
